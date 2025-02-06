f8c208620cbb93c0271b8ee6b02f367d23acabd0
Namaloom4474 <ghanam@smartcarship.com>
2025-02-06 22:41:42 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,25 +1,27 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const path = require("path");
+
 const app = express();
-
-app.use(express.static("public"));
-
 const commissionCalculator = require("./routes/commissionCalculator.js");
 
 const PORT = process.env.PORT || 3000;
 
-
-
+app.use(express.static(path.join(__dirname, "public")));
 
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json()); // Allow JSON parsing
 
 app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/public/index.html");
+  res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
 app.post("/api/calculate", commissionCalculator.calculateSalary);
 
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
-});
+if (require.main === module) {
+  app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+  });
+}
+
 module.exports = app;
