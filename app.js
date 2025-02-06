8c465a480dc577cc07b054d8f7b0a96baf4e588e
Namaloom4474 <ghanam@smartcarship.com>
2025-02-06 23:19:14 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,27 +1,15 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const path = require("path");
+const serverless = require("serverless-http"); // Add this
 
 const app = express();
 const commissionCalculator = require("./routes/commissionCalculator.js");
 
-const PORT = process.env.PORT || 3000;
-
-app.use(express.static(path.join(__dirname, "public")));
-
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json()); // Allow JSON parsing
+app.use(bodyParser.json());
 
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "index.html"));
-});
 
 app.post("/api/calculate", commissionCalculator.calculateSalary);
 
-if (require.main === module) {
-  app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
-  });
-}
-
-module.exports = app;
+module.exports.handler = serverless(app); // Replace `module.exports = app`
